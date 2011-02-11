03f572b3d2c44545307d5ba46f6dd9cbade80bc8
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-02-11 14:12:54 -0700

    modified mult.asm again

--- project04/mult/Mult.asm
+++ project04/mult/Mult.asm
@@ -8,30 +8,24 @@
   D=A
   @R1
   M=D
-  
 (PRE)
   @0
   D=A
   @R2
   M=D
-  
 (LO)
   @R0            
   D=M
   @OVR
   D;JLE //if < 0, jump to OVR
-
   @R0          
   M=M-1 //decrement R0
-
   @R1             
   D=M
   @R2
   M=M+D //Add R1 to R2
-
   @LO         
   0;JMP //Goto LO
-
 (OVR)
   @OVR	
   0;JMP	//INF Loop once done
