8cde18d9c76045646d6a3406762d3b9b9d97a05b
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-02-11 13:04:10 -0700

    Adding the finish mult.asm and .hack files

---
+++ project04/mult/Mult.asm
@@ -0,0 +1,35 @@
+@PRE
+  0;JMP
+  @0
+  D=A
+  @R0
+  M=D
+  @2
+  D=A
+  @R1
+  M=D
+(PRE)
+  @0
+  D=A
+  @R2
+  M=D
+(LO)
+  @R0            
+  D=M
+  @OVR
+  D;JLE //if < 0, jump to OVR
+
+  @R0          
+  M=M-1 //decrement R0
+
+  @R1             
+  D=M
+  @R2
+  M=M+D //Add R1 to R2
+
+  @LO         
+  0;JMP //Goto LO
+
+(OVR)
+  @OVR	
+  0;JMP	//INF Loop once done
