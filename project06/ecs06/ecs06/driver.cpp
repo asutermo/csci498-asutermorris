a90aba19a63603a7a7964b9dac9b276882b63be3
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-02-20 20:12:49 -0700

    Adding initial project files - also configured file to accept command line args

---
+++ project06/ecs06/ecs06/driver.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+	{
+		exit(1);
+	}
+	char *fileName = argv[1];
+
+
+
+}
