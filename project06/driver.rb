d207fe82ebb14238f2f856b468cd3c280c03740a
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-02-22 21:00:12 -0700

    Ruby is now working. outputs proper file, closes IO streams, etc

--- project06/driver.rb
+++ project06/driver.rb
@@ -1,12 +1,14 @@
-inFile = File.open(ARGV[0],'r');
+instr = ARGV[0]
+inFile = File.open(instr,'r');
 
 if File.fnmatch('*', '.asm', File::FNM_DOTMATCH) 
 	puts "Success in finding file"
+	of = instr.dup
+	of["asm"] = "hack"
+	outFile = File.open(of, 'w');
+	inFile.close
+	outFile.close
 else
 	puts "File doesn't exist, proper argument was not supplied or invalid file name given"
 	Process.exit
 end
-of = ARGV[0] + ".hack"
-outFile = File.open(of, 'w');
-inFile.close
-outFile.close
