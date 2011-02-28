20771b27d07f14c92d3a0e3f34ac24c462166ff2
Andrew Suter-Morris <asutermo@mymail.mines.edu>
2011-02-27 18:13:49 -0700

    Now passing everything again. victory

--- project06/assembler.rb
+++ project06/assembler.rb
@@ -113,16 +113,12 @@ comp = {'0'=> '0101010', '1'=> '0111111', '-1'=> '0111010', 'D'=> '0001100',
 '-M'=> '1110011', 'M+1'=> '1110111',
 'M-1'=> '1110010', 'D+M'=> '1000010', 'D-M'=> '1010011', 'M-D'=> '1000111',
 'D&M'=> '1000000', 'D|M'=> '1010101'}
-	puts "begin comp: " + input
-	
-	code = input.gsub(/(^.*\=)|(;.*%)/, '')
-	puts "end comp: " + comp[code]
+	code = input.gsub(/(^.*\=)|(;.*$)/, '')
 	return comp[code]
 end
 
 #dest
 def dest(input)
-	puts "begin dest" + input
 dest = {'M'=> '001', 'D'=> '010', 'MD'=> '011', 'A'=> '100', 
 'AM'=> '101', 'AD'=> '110', 'AMD'=> '111'}
 	if input.match('=')
@@ -136,11 +132,13 @@ end
 	
 #jump
 def jump(input)
-	puts input
+	puts "begin jump: " + input
 jmp = {'JGT'=> '001', 'JEQ'=> '010', 'JGE'=> '011', 'JLT'=> '100', 
 'JNE'=> '101', 'JLE'=> '110', 'JMP'=> '111'}
 	if input.match(';')
-		code = input.gsub(/^.*;/, '')
+		puts "match"
+		code = input.gsub(/.*;/, '')
+		puts code
 		return jmp[code]
 	else
 		return '000'
